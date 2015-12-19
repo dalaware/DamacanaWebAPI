e7b42a0a6770f4f38a3d38f164a932d934f0a1b5
Enes Beşinci <dalaware@gmail.com>
2015-12-19 21:08:35 +0200

    Models and Controllers Added
    Commit 2

---
+++ DamacanaWebAPI/Models/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamacanaWebAPI.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<Product> Cartproducts { get; set; }
+
+        public virtual Product Product { get; set; }
+
+    }
+}
